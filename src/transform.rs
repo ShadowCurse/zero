7714a10a38ac220d52a3950abaeedaad3eac12b5
ShadowCurse <48793978+ShadowCurse@users.noreply.github.com>
2024-03-26 22:33:11 +0000

    add Copy and conversion to Matrix4 to Transform

--- src/transform.rs
+++ src/transform.rs
@@ -23,11 +23,19 @@ impl From<&Transform> for TransformUniform {
     }
 }
 
-#[derive(Debug, Clone)]
+#[derive(Debug, Clone, Copy)]
 pub struct Transform {
     pub translation: Vector3<f32>,
     pub rotation: Quaternion<f32>,
     pub scale: Vector3<f32>,
+}
+
+impl From<&Transform> for Matrix4<f32> {
+    fn from(value: &Transform) -> Self {
+        Matrix4::from_translation(value.translation)
+            * Matrix4::from(value.rotation)
+            * Matrix4::from_nonuniform_scale(value.scale.x, value.scale.y, value.scale.z)
+    }
 }
 
 impl Default for Transform {
